a0e1c7d3f7ef66171d348d9d42ccb7c91df3b95d
cjp34 <carlparm@gmail.com>
2020-03-13 15:49:46 -0400

    Need to add like button

--- src/index.js
+++ src/index.js
@@ -14,13 +14,33 @@ function renderDogs(input) {
     let dogObject = input;
     let spanBar = document.createElement('span')
         spanBar.innerText = input.name
-        spanBar.addEventListener('click', clickDog)
+        spanBar.id = input.id
+        spanBar.addEventListener('click', clickGetDog)
     let dogBar = document.querySelector('#dog-bar')
 
     dogBar.appendChild(spanBar)
 
 }
 
-function clickDog(event) {
-    debugger;
+function clickGetDog(event) {
+    fetch(`http://localhost:3000/pups/${event.target.id}`)
+    .then(resp => resp.json())
+    .then(json => getDog(json))
+}
+
+function getDog(dog) {
+    let dogLocation = document.getElementById('dog-info')
+    dogLocation.innerHTML = ''
+    let dogShow = document.createElement('div')
+        let dogName = document.createElement('h2')
+            dogName.innerText = dog.name
+        let dogImage = document.createElement('img')
+            dogImage.src = dog.image
+            dogShow.append(dogImage, dogName)
+            dogLocation.append(dogShow)        
+            if ( dog.idGoodDog === 'false' ) {
+            let gBB = document.createElement('button')
+            gBB.innerText = "Good Dog"
+            dogShow.append(gBB)
+        }
 }
