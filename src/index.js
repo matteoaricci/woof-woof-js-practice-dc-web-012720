0e879bd060420036085affa86426d8871ae2d9ff
matteoaricci <matteo.a.ricci@gmail.com>
2020-03-13 15:09:01 -0400

    Up to Step 2 Done

---
+++ src/index.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetchDogs();
+})
+
+function fetchDogs() {
+    fetch('http://localhost:3000/pups')
+    .then(resp => resp.json())
+    .then(resp => resp.forEach(element => {
+        renderDogs(element)
+    }))
+}
+
+function renderDogs(input) {
+    let spanBar = document.createElement('span')
+        spanBar.innerText = input.name
+        spanBar.addEventListener('click', clickDog)
+    let dogBar = document.querySelector('#dog-bar')
+
+    dogBar.appendChild(spanBar)
+
+}
+
+function clickDog() {
+    console.log("I've been clicked!")
+}
